58bb4228076e1d25c71aea2f62b150e811de0990
山根　直也 <yamamesan708@gmail.com>
2022-05-03 20:55:51 +0900

    add items controller

---
+++ test/controllers/items_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ItemsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get items_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get items_edit_url
+    assert_response :success
+  end
+end
