1daab4b14055bdca383af42f1fd4484010b1be2d
山根　直也 <yamamesan708@gmail.com>
2022-05-03 22:05:00 +0900

    add 在庫表 & bootstrap5導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+items = [
+    ["ハチクサン", 100, "L"],
+    ["TMカウンター", 200, "g"],
+    ["ブロワー", 20, "個"],
+    ["かわい太郎", 50, "坪"]
+]
+
+items.each do |name, stock, unit|
+    Item.create(name: name, stock: stock, unit: unit)
+end
