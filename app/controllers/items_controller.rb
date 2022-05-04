ee6715467c477ffc9031545bc4d27155a2e7a28b
山根　直也 <yamamesan708@gmail.com>
2022-05-04 23:34:06 +0900

    出庫処理を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,60 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(name: new_item_params[:name], 
+            stock: new_item_params[:stock], unit: new_item_params[:unit])
+      update_flash
+      redirect_to items_path
+    else
+      render_form
+    end
+      
   end
+  
+  def out_stock
+    @item = Item.find(params[:id])
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:id, :name, :stock, :unit, :process_number)
+    end
+
+    # item_params[:stock]を出庫ならマイナス入庫ならプラスしたものを返す、その他編集なら何もしないものを返す
+    # mergeメソッドでitem_paramsの値を変更したものをnew_item_paramsとして返す
+    def new_item_params
+      if params[:commit] == "出庫"
+        new_stock = (item_params[:stock].to_i - item_params[:process_number].to_i).to_s
+        item_params.merge(:stock => new_stock)
+      elsif params[:commit] == "入庫"
+        new_stock = (item_params[:stock].to_i + item_params[:process_number].to_i).to_s
+        item_params.merge(:stock => new_stock)
+      end
+    end
+
+    # パラメータによって、入庫、出庫などの処理によってフラッシュメッセージを使い分ける
+    def update_flash
+      process_word = "#{@item.name}を#{item_params[:process_number]}#{@item.unit}"
+      if params[:commit] == "出庫"
+        flash[:success] = process_word + "出庫しました"
+      elsif params[:commit] == "入庫"
+        flash[:success] =  process_word + "入庫しました"
+      else
+        flash[:success] = "編集が完了しました"
+      end
+    end
+
+    # パラメータによって、入庫、出庫などの処理によってrender先が変わる
+    def render_form
+      if params[:commit] == "出庫"
+        render 'out_stock'
+      elsif
+        params[:commit] == "入庫"
+        render 'in_stock'
+      else
+        render 'edit'
+      end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index, :edit, :update]
+  resources :items, only: [:index, :update]
+  # 出庫
+  resources :items do
+    member do
+      get 'out_stock' 
+    end
+  end
+  # get 'items/out_stock', to: 'items#out_stock'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
