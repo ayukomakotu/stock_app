41a64a5979b3c46ac86515fe02f0a983d4c989fb
山根　直也 <yamamesan708@gmail.com>
2022-05-04 10:06:37 +0900

    add models/item_spec.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :stock, presence: true
+    validates :unit, presence: true
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+    before do 
+        @item = FactoryBot.build(:sample1)
+    end
+
+    context 'バリデーション' do
+        it 'itemが有効になるか' do
+            expect(@item).to be_valid
+        end
+
+        it 'nameが空で無効になるか' do
+            @item.name = "    "
+            expect(@item).to be_invalid
+        end
+
+        it "nameの一意性は担保されているか" do
+            @item.save
+            dup_item = @item.dup
+            expect(dup_item).to be_invalid
+        end
+
+        it "stockが空で無効になるか" do
+            @item.stock = nil
+            expect(@item).to be_invalid
+        end
+
+        it "unitが空で無効になるか" do
+            @item.unit = nil
+            expect(@item).to be_invalid
+        end
+    end
+end
