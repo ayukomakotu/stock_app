15a20e7caf4c292445e7bed0ccf5b5613f5b7fb4
山根　直也 <yamamesan708@gmail.com>
2022-05-05 21:32:34 +0900

    add request/item_controller_spec.rb

--- spec/requests/items_controller_spec.rb
+++ spec/requests/items_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "ItemsControllers", type: :request do
+  
   let!(:sample1)  { FactoryBot.create(:sample1) }
   let!(:sample2)  { FactoryBot.create(:sample2) }
 
@@ -9,11 +10,12 @@ RSpec.describe "ItemsControllers", type: :request do
       get items_path
       expect(response.status).to eq 200
     end
+  end
 
-    it '在庫表が表示されているか' do
-      get items_path 
-      expect(response.body).to include sample1.name
-      expect(response.body).to include sample2.name
+  describe "GET :id/out_stock" do
+    it "リクエストが成功するか" do
+      get out_stock_item_path(sample1)
+      expect(response.status).to eq 200
     end
   end
 end
