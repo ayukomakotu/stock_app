4a94a9dabda5c76a99c6f52139d964bf7046ae4e
yamane naoya <yamanenaoya@yamanenoMacBook-Air.local>
2022-05-12 14:16:35 +0900

    seeds.rb編集 report追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,21 @@ items.each do |name, stock, unit|
     item = Item.create(name: name, unit: unit)
     Stock.create(number: stock, item: item)
 end
+
+# ユーザーをまとめて生成する
+10.times do |n|
+  User.create!(name: Faker::Name.name, number: n)
+end
+
+user = User.create!(name: "山根", number: 0202)
+
+(1..3).each do |n|
+    Report.create(user_id: User.find(n), item_id: Item.find(n), process: "出庫", day: Date.today,
+                purpose: "コダマ邸使用のため", confirmer: nil, confirmation: false)
+    Report.create(user_id: User.find(n), item_id: Item.find(n), process: "出庫", day: Date.today,
+                purpose: nil, confirmer: user.id, confirmation: true)
+    Report.create(user_id: User.find(n), item_id: Item.find(n), process: "入庫", day: Date.today,
+                purpose: "コダマ邸使用のため", confirmer: nil, confirmation: false)
+    Report.create(user_id: User.find(n), item_id: Item.find(n), process: "入庫", day: Date.today,
+                purpose: nil, confirmer: user.id, confirmation: true)
+end
