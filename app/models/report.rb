2dce9554abf026faa87243443180361cad8a33fd
ayukomakotu <yamamesan708@gmail.com>
2022-05-14 20:54:16 +0900

    confirmer in report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :user
+  #confrimer_idを参照して,対応するuserを表示
+  belongs_to :confirmer, class_name: "User",
+                        foreign_key: "confirmer_id"
   belongs_to :item
   validates :process, presence: true
   validates :process_number, presence: true
