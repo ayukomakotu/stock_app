c34b947870e83a8ce63b96c617bd464ccada0323
ayukomakotu <yamamesan708@gmail.com>
2022-05-18 23:00:13 +0900

    improve sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(number: params[:session][:number])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to items_path
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
