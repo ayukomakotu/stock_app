34244effd40f665d07e5a40ce4d13b5640f746a7
ayukomakotu <yamamesan708@gmail.com>
2022-05-18 22:44:07 +0900

    ログインフォーム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :reports, only: :index
   resources :items, only: [:index, :edit, :update]
   resources :stocks, only: [:edit, :update]
