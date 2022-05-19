0aea933154c318d8dbc3e8de9fc46921595a8259
ayukomakotu <yamamesan708@gmail.com>
2022-05-19 22:27:27 +0900

    add password_digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_070253) do
+ActiveRecord::Schema.define(version: 2022_05_19_131858) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_05_15_070253) do
     t.integer "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["number"], name: "index_users_on_number", unique: true
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
     factory :user1, class: User do
         name  { "user1" }
         number { 1000 }
+        password_digest { User.digest('password') }
     end
 end
