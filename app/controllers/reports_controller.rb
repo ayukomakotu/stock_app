f1e099104043f1296d8dae453c51c2693e9061b5
yamane naoya <yamanenaoya@yamanenoMacBook-Air.local>
2022-05-28 15:48:09 +0900

    add report confirm ajaxなし

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,6 +8,7 @@ class ReportsController < ApplicationController
   def update
     @report = Report.find(params[:id])
     if @report.update(report_params)
+      update_flash
       redirect_to reports_path
     else
       render 'index' 
@@ -18,5 +19,16 @@ class ReportsController < ApplicationController
       def report_params
         params.require(:report).permit(:user_id, :item_id, :process, :process_number,
                         :day, :purpose, :confirmer_id, :confirmation)
+      end
+
+      # パラメータによって、入庫、出庫などの処理によってフラッシュメッセージを使い分ける
+      def update_flash
+        if params[:commit] == "未入力"
+          flash[:success] = "顧客管理ソフトへの入力を確認しました"
+        elsif params[:commit] == "入力済"
+          flash[:success] = "顧客管理ソフトへの入力を未入力に戻しました"
+        else
+          flash[:success] = "履歴を編集しました"
+        end
       end
 end
--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Reports", type: :request do
+  let!(:user1)  { FactoryBot.create(:user1)}
   describe "GET /index" do
     it "リクエストが成功するか" do
+      log_in_as(user1)
       get reports_path
       expect(response).to have_http_status(:success)
     end
