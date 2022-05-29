8d2f99fd97239564a4641c9421057475978ddc1f
ayukomakotu <yamamesan708@gmail.com>
2022-05-29 16:29:12 +0900

    add header_spec

---
+++ spec/system/header_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Header", type: :system do
+    let!(:user1)  { FactoryBot.create(:user1) }
+
+    context 'ログイン状態' do
+        it "リンクが正しいか" do
+            log_in_system(user1)
+            find(".navbar-brand").click
+            expect(current_path).to eq root_path
+            click_on "在庫表"
+            expect(current_path).to eq items_path
+            click_on "処理履歴"
+            expect(current_path).to eq reports_path
+            click_on "ログアウト"
+            expect(current_path).to eq login_path
+            expect(page).not_to have_link 'ログイン', href: login_path
+        end
+    end
+
+    context '非ログイン状態' do
+        it "リンクが正しいか" do
+            visit login_path
+            find(".navbar-brand").click
+            expect(current_path).to eq root_path
+            expect(page).not_to have_link '在庫表', href: items_path
+            expect(page).not_to have_link '処理履歴', href: reports_path
+            expect(page).not_to have_link 'ログアウト', href: logout_path
+            click_on "ログイン"
+            expect(current_path).to eq login_path
+        end
+    end
+end
+
