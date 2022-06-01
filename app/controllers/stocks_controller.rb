ec2d229156bf528284406e6081ee95521f64ded6
ayukomakotu <yamamesan708@gmail.com>
2022-06-01 22:39:14 +0900

    在庫処理時に履歴が作成される仕様に変更 add stock_form.rb improve stocks_controller stocks/edit

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,18 @@ class StocksController < ApplicationController
   
   def edit
     @stock = Stock.find(params[:id])
+    @item = @stock.item
+    @stock_form = StockForm.new
   end
 
   def update
     @stock = Stock.find(params[:id])
     @item = Item.find(@stock.item_id)
+    @stock_form = StockForm.new(stock_form_params)
     # presentはnil, "", " ", [], {}でなければtrueを返す
-    if params[:stock][:process_number].present?
+    if params[:stock_form][:process_number].present?
       if @stock.update(number: new_stock)
+        @stock_form.save
         update_flash
         redirect_to items_path
       else
@@ -26,22 +30,27 @@ class StocksController < ApplicationController
   end
 
   private
-    def stock_params
-      params.require(:stock).permit(:process_number)
+    # def stock_params
+    #   params.require(:stock).permit(:process_number)
+    # end
+
+    def stock_form_params
+      params.require(:stock_form).permit(:day, :process, :process_number, :purpose, 
+                                                :number, :item_id, :user_id, :id)
     end
 
     # 入出庫の計算　出庫ならstockからprocess_numberをひいた値を返す、入庫なら足した値を返す
     def new_stock
       if params[:commit] == "出庫"
-        @stock.number - stock_params[:process_number].to_i
+        @stock.number - stock_form_params[:process_number].to_i
       elsif params[:commit] == "入庫"
-        @stock.number + stock_params[:process_number].to_i
+        @stock.number + stock_form_params[:process_number].to_i
       end
     end
 
     # パラメータによって、入庫、出庫などの処理によってフラッシュメッセージを使い分ける
     def update_flash
-      process_word = "#{@item.name}を#{stock_params[:process_number]}#{@item.unit}"
+      process_word = "#{@item.name}を#{stock_form_params[:process_number]}#{@item.unit}"
       if params[:commit] == "出庫"
         flash[:success] = process_word + "出庫しました"
       elsif params[:commit] == "入庫"
--- app/models/stock_form.rb
+++ app/models/stock_form.rb
@@ -1,5 +1,14 @@
 class StockForm
-include ActiveModel::model
-# StockFormでメソッドとして使えるようにする
-attr_accessor :day, :process, :process_number, :purpose, :confirmer_id,
-            :confirmation, :number, :item_id, :user_id
+    include ActiveModel::Model
+    # StockFormでメソッドとして使えるようにする
+    attr_accessor :day, :process, :process_number, :purpose, :number, :item_id, :user_id, :id
+
+    def save
+        Report.create(user_id: user_id, item_id: item_id, process: process, 
+                process_number: process_number, day: day, purpose: purpose)
+    end
+
+
+
+
+end
