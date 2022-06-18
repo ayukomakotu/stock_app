09006d7f37c0a96853dc53ac817718ee26175c8c
ayukomakotu <yamamesan708@gmail.com>
2022-06-18 22:27:45 +0900

    item_new_spec修正

--- spec/system/item_new_spec.rb
+++ spec/system/item_new_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "ItemNew", type: :system do
     it "正しい値を入力すると登録できるか" do
         log_in_system(user1)
         visit new_item_path
-        fill_in "item[name]", with: "sample"
-        fill_in "item[stock]", with: 30
-        fill_in "item[unit", with: "個"
+        fill_in "item_form[name]", with: "sample"
+        fill_in "item_form[number]", with: 30
+        fill_in "item_form[unit]", with: "個"
         click_on "登録"
         expect(page).to have_content "sample"
         expect(page).to have_content 30
@@ -18,19 +18,22 @@ RSpec.describe "ItemNew", type: :system do
     it "不正な値を入力すると、失敗するか" do
         log_in_system(user1)
         visit new_item_path
-        fill_in "item[name]", with: "    "
-        fill_in "item[stock]", with: 30
-        fill_in "item[unit", with: "個"
+        fill_in "item_form[name]", with: "    "
+        fill_in "item_form[number]", with: 30
+        fill_in "item_form[unit]", with: "個"
+        click_on "登録"
         expect(page).to have_selector "#error_explanation"
 
-        fill_in "item[name]", with: "sample"
-        fill_in "item[stock]", with: nil
-        fill_in "item[unit", with: "個"
+        fill_in "item_form[name]", with: "sample"
+        fill_in "item_form[number]", with: nil
+        fill_in "item_form[unit]", with: "個"
+        click_on "登録"
         expect(page).to have_selector "#error_explanation"
 
-        fill_in "item[name]", with: "sample"
-        fill_in "item[stock]", with: nil
-        fill_in "item[unit", with: "  "
+        fill_in "item_form[name]", with: "sample"
+        fill_in "item_form[number]", with: nil
+        fill_in "item_form[unit]", with: "  "
+        click_on "登録"
         expect(page).to have_selector "#error_explanation"
     end
 end
